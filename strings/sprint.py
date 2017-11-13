69609ebcf7b178dee22fb995b8e7306e104a03fa
nshotts392 <nshotts392@lsr7.net>
2017-11-13 11:01:55 -0600

    more

--- strings/sprint.py
+++ strings/sprint.py
@@ -3,16 +3,16 @@ from mcpi.minecraft import Minecraft
 mc = Minecraft.create()
 
 pos1 = mc.player.getTilePos()
-x1 = pos1.x
-y1 = pos1.y
-z1 = pos1.z
+x1 = -1
+y1 = 8
+z1 = 8.3
 
 time.sleep(10)
 
 pos2 = mc.player.getTilePos()
-x2 = pos2.x
-y2 = pos2.y
-z2 = pos2.z
+x2 = 2
+y2 = 50
+z2 = 7
 
 xDistance = x2 - x1
 yDistance = y2 - y1
