2301dfbb4e40bc6765988d1e2851dd0d34d73135
nshotts392 <nshotts392@lsr7.net>
2017-11-20 10:56:46 -0600

    fixed

--- ifStatements/crater.py
+++ ifStatements/crater.py
@@ -1,9 +1,17 @@
 from mcpi.minecraft import Minecraft
 mc = Minecraft.create()
 
-password = "Y"
-answer = input("Create a crater? Y/N: ")
-if answer == password:
+boom = "Y"
+noBoom = "N"
+attempt = raw_input("Create a crater? Y/N: ")
+if attempt == boom:
+    print("Boom!")
     pos = mc.player.getPos()
     mc.setBlocks(pos.x + 1, pos.y + 1, pos.z + 1, pos.x - 1, pos.y - 1, pos.z -1, 0)
     mc.postToChat("Boom!")
+if attempt == noBoom:
+    print("Why not?")
+    
+
+    
+    
--- strings/The_Input()_Fuction.py
+++ strings/The_Input()_Fuction.py
@@ -1,8 +1,3 @@
-Python 3.4.2 (default, Oct 19 2014, 13:31:11) 
-[GCC 4.9.1] on linux
-Type "copyright", "credits" or "license()" for more information.
->>> name = input("What is your name?")
-What is your name? Amanda
->>> print(name)
- Amanda
->>> 
+name = input("What is your name?")
+print(name)
+
