baedf93d5643c8051d42dde91b0eb0a8b8b9868a
nshotts392 <nshotts392@lsr7.net>
2017-11-21 11:07:05 -0600

    lava

--- ifStatements/secretDoor.py
+++ ifStatements/secretDoor.py
@@ -1,4 +1,14 @@
-Python 3.4.2 (default, Oct 19 2014, 13:31:11) 
-[GCC 4.9.1] on linux
-Type "copyright", "credits" or "license()" for more information.
->>> 
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+
+x = 68.7
+y = 8.0
+z = -32.3
+
+gift = mc.getBlock(x, y, z)
+if gift == 57:
+    confirm = input("Do you want to enter: ")
+    if confirm == "yes":
+        mc.setBlock(x = 65.3, y = 5.9, z = -29.5, 0)
+else:
+    mc.postToChat("Place an offering on the pedestal.")
