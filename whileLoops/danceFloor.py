e355032eb3bf6d1ca22d112ae3572c40833990cc
awhiteman625 <awhiteman625@lsr7.net>
2017-11-30 10:44:54 -0600

    dance

--- whileLoops/danceFloor.py
+++ whileLoops/danceFloor.py
@@ -11,8 +11,15 @@ length = 5
 block = 41
 mc.setBlocks(floorX, floorY, floorZ, floorX + width, floorY, floorZ + length, block)
 
-while floorX <= pos.x <= floorX + width and floorY <= pos.y <= floorY and floorZ <= pos.z <= floorZ + length:
+while floorX <= pos.x <= floorX + width and floorZ <= pos.z <= floorZ + length:
+    print("program working")
     if block == 41:
         block = 57
+        print("program working")
     else:
         block = 41
+        print("worked!")
+    mc.setBlocks(floorX, floorY, floorZ, floorX + width, floorY, floorZ + length, block)
+    pos = mc.player.getTilePos()
+    time.sleep(0.5)
+
