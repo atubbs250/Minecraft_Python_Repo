0c94e45649963bb39882e216524f5571b8a1523e
awhiteman625 <awhiteman625@lsr7.net>
2017-11-30 11:07:27 -0600

    m

---
+++ whileLoops/chatLoop.py
@@ -0,0 +1,5 @@
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+message = "True! - nervous - very very dreadfully nervous for I had been and am, but why will you say that I am mad?"
+mc.postToChat(message)
+
