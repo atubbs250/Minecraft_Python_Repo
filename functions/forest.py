08d1a0538818a24be31deb5e9e9bc343b78f7f4b
awhiteman625 <awhiteman625@lsr7.net>
2017-12-04 10:57:26 -0600

    morenotfinishedIdontknowhow

--- functions/forest.py
+++ functions/forest.py
@@ -1,10 +1,14 @@
 from mcpi.minecraft import Minecraft
 mc = Minecraft.create()
 
+tree = 6
 def growTree(x, y, z):
+    x = -2.9
+    y = 8.0
+    z = -2.9
+    mc.setBlocks(x, y, z, 6)
+    print("tree!")
     
-
-
 pos = mc.player.getTilePos()
 x = pos.x
 y = pos.y
