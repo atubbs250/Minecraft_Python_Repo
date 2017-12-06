db60456b76636f09f56c9e7589660ecd4cedd41c
awhiteman625 <awhiteman625@lsr7.net>
2017-12-06 11:04:25 -0600

    more

--- functions/forest.py
+++ functions/forest.py
@@ -1,19 +1,30 @@
 from mcpi.minecraft import Minecraft
 mc = Minecraft.create()
 
-tree = 6
+x = -6.6
+y = 0.0
+z = -59.1
+TreeStart = mc.getBlock(x+1, y, z)
 
 def growTree():
     tree = 6
-    pos = mc.player.getPos()
-    x = pos.x
-    y = pos.y
-    z = pos.z
     mc.setBlock(x+1, y, z, 6)
     print("tree!")
-    
+    x += 1
+def growForest():
+    x = x + 1
+    y = pos.y
+    z = pos.z
+    for i in range(5):
+        tree = 6
+        x = x + 1
+        mc.setBlock(x, y, z, 6)
+        print("tree!")
+
 growTree()
 growTree()
+growTree()
+
     
 
 
