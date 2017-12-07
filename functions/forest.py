7392737422d522b89796cacfc32f760fe7d4514a
awhiteman625 <awhiteman625@lsr7.net>
2017-12-07 10:33:24 -0600

    finally

--- functions/forest.py
+++ functions/forest.py
@@ -1,30 +1,17 @@
 from mcpi.minecraft import Minecraft
 mc = Minecraft.create()
 
-x = -6.6
-y = 0.0
-z = -59.1
-TreeStart = mc.getBlock(x+1, y, z)
+x = 0.5
+y = 7.0
+z = 0.5
+count = 1
 
 def growTree():
     tree = 6
     mc.setBlock(x+1, y, z, 6)
     print("tree!")
+
+while count <= 6:
+    growTree()
     x += 1
-def growForest():
-    x = x + 1
-    y = pos.y
-    z = pos.z
-    for i in range(5):
-        tree = 6
-        x = x + 1
-        mc.setBlock(x, y, z, 6)
-        print("tree!")
-
-growTree()
-growTree()
-growTree()
-
-    
-
-
+    count = count + 1
