390e570f7b8329785d3c2792d711357bf58a37e5
atubbs250 <atubbs250@lsr7.net>
2018-01-29 11:00:37 -0600

    tuna

--- classes/catClass.py
+++ classes/catClass.py
@@ -3,7 +3,15 @@ class Cat(object):
         self.name = name
         self.weight = weight
 
-fluff = Cat("Fluff", 4.5)
-fluff.weight = 5
+    def eat(self, food):
+        self.weight = self.weight + 0.05
+        print(self.name + " is eating " + food)
 
+    def eatAndSleep(self, food):
+        self.eat(food)
+        print(self.name + " is now sleeping...")
+
+fluff = Cat("Fluff", 4.5)
 print(fluff.weight)
+fluff.eat("tuna")
+fluff.eatAndSleep("tuna")
