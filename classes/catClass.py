4624a99f2a01bfe2d79881e56ff875888e28e0e9
atubbs250 <atubbs250@lsr7.net>
2018-01-30 10:36:12 -0600

    cat

--- classes/catClass.py
+++ classes/catClass.py
@@ -3,6 +3,9 @@ class Cat(object):
         self.name = name
         self.weight = weight
 
+    def getWeightInGrams(self):
+        return self.weight * 1000
+
     def eat(self, food):
         self.weight = self.weight + 0.05
         print(self.name + " is eating " + food)
@@ -15,3 +18,4 @@ fluff = Cat("Fluff", 4.5)
 print(fluff.weight)
 fluff.eat("tuna")
 fluff.eatAndSleep("tuna")
+print(fluff.getWeightInGrams())
